32b92c880fd6e6f4d3875c65340b49efc6c2a3bd
NikiKaloyanov <nikolaikaloianov@gmail.com>
2023-03-25 21:13:19 +0200

    Sign in page without api call / introduced loader design (To be implemented)

---
+++ costi-fe/src/Components/Loader/index.tsx
@@ -0,0 +1,28 @@
+import "./Loader.css";
+
+//TODO: Loader Component
+const Loader = () => {
+    //DO NOT USE Absolute!!!
+    //Placeholder
+    return (
+        <>
+            <div className="circle-container">
+                <span className="circle1 circle"></span>
+                <span className="circle2 circle"></span>
+                <span className="circle3 circle"></span>
+                <span className="circle4 circle shadow"></span>
+                <span className="circle5 circle shadow"></span>
+                <span className="circle6 circle shadow"></span>
+
+                {/*<span className="circle7 circle"></span>*/}
+                {/*<span className="circle8 circle"></span>*/}
+                {/*<span className="circle9 circle"></span>*/}
+                {/*<span className="circle10 circle"></span>*/}
+                {/*<span className="circle11 circle"></span>*/}
+                {/*<span className="circle12 circle"></span>*/}
+            </div>
+        </>
+    )
+}
+
+export default Loader;
